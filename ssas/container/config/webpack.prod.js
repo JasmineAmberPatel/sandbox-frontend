f55c470519bee054b73880139529f3de1ca6a8b8
Luisa Pinto <luisa.pinto@footasylum.com>
2021-04-28 16:28:31 +0100

    Webpacks PublicPath Setting

--- ssas/container/config/webpack.prod.js
+++ ssas/container/config/webpack.prod.js
@@ -8,7 +8,8 @@ const domain = process.env.PRODUCTION_DOMAIN
 const prodConfig = { 
     mode: 'production', // makes sure JS files are optimised and minified
     output: {
-        filename: '[name].[contenthash].js' // whenever we build some files for production every file uses this template for naming
+        filename: '[name].[contenthash].js', // whenever we build some files for production every file uses this template for naming
+        publicPath: '/container/latest/'
     },
     plugins: [
         new ModuleFederationPlugin({
